1d5f197d0b41a13836c63006e88f0c125e445dcc
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2022-02-26 17:03:48 -0600

    refactoring handlers.go

--- handlers.go
+++ handlers.go
@@ -13,13 +13,9 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
-	todos := Todos{
-		Todo{Name: "Write presentation"},
-		Todo{Name: "Host meetup"},
-	}
-
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+
 	if err := json.NewEncoder(w).Encode(todos); err != nil {
 		panic(err)
 	}
