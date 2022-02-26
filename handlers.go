caadd040cac81ac44ba6215d6ef3c953cfe92b19
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2022-02-26 16:57:26 -0600

    updating handler.go

--- handlers.go
+++ handlers.go
@@ -18,6 +18,8 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 		Todo{Name: "Host meetup"},
 	}
 
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(todos); err != nil {
 		panic(err)
 	}
