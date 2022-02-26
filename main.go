4ca2a9952e69cb8f632bf4001eeceb21d5922e09
Whitney Lampkin <18070772+WhitneyLampkin@users.noreply.github.com>
2022-02-26 16:31:39 -0600

    adding github.com/gorilla/mux module

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
- 
+
 import (
-    "fmt"
-    "html"
-    "log"
-    "net/http"
- 
-    "github.com/gorilla/mux"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
- 
+
 func main() {
- 
-    router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", Index)
-    log.Fatal(http.ListenAndServe(":8080", router))
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
- 
+
 func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
